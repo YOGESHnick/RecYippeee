38cde26bd157af77003a212101c0539c60b07d84
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-09-28 18:19:15 +0530

    exploring routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import {userRouter} from "./routes/users.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/auth",userRouter);
 
 mongoose.connect(
     "mongodb+srv://yogeshshamlin:Nickyzander%40%32%30%30%34@cluster0.4a6cuu0.mongodb.net/RecYipeee?retryWrites=true&w=majority"
