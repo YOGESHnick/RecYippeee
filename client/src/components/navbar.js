affbaffdc41449eecec464e215086c095f5e6128
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-01 10:18:21 +0530

    styled a basic navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,12 +1,18 @@
 import {Link} from "react-router-dom";
+import 'font-awesome/css/font-awesome.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 export const Navbar = ()=>{
     return (
         <div className="navbar">
-            <Link to="/" >Home</Link> 
-            <Link to="/create-recipe" >Create a RecYippee!</Link> 
-            <Link to="/saved-recipes" >Saved RecYippees</Link> 
-            <Link to="/auth" >Login/Register</Link> 
+            <Link to="/" ><i className="fa fa-home"></i>Home</Link> 
+            <Link to="/create-recipe" ><FontAwesomeIcon icon={faUtensils} />Create a RecYippee!</Link> 
+            <Link to="/saved-recipes" ><FontAwesomeIcon icon={faBookmark} />Saved RecYippees</Link> 
+            <Link to="/auth" ><FontAwesomeIcon icon={faSignInAlt} />Login/Register</Link> 
         </div>
     )
 }
