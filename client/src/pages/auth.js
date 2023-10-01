d3525570bf785522e4627726b7067437180d250f
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-01 20:22:46 +0530

    updating login/register functions

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,5 +1,36 @@
+import {useState} from "react";
+
 export const Auth = ()=>{
     return (
-        <div>Auth</div>
+        <div className="auth">
+            <Login />
+            <Register />
+        </div>
+    )
+}
+
+const Login = ()=>{
+    return (
+        <div> </div>
+    )
+}
+
+const Register = ()=>{
+    const [username,setUsername]  = useState("");
+    const [password,setPassword]  = useState("");
+    return (
+        <div className="auth-container"> 
+            <form>
+                <h2>Register</h2>
+                <div className="form-group">
+                    <label htmlFor="username">Username : </label>
+                    <input type="text" id="username" value={username} onChange={(event)=>setUsername(event.target.value) } />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="username">Password : </label>
+                    <input type="text" id="password" value={password} onChange={(event)=>setPassword(event.target.value) } />
+                </div>
+            </form>
+        </div>
     )
 }
