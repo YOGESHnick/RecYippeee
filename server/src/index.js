cbe02101306950e8e2fb5e3fcdacac1f5d0ae293
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-01 21:15:32 +0530

    new start port

--- server/src/index.js
+++ server/src/index.js
@@ -12,6 +12,6 @@ mongoose.connect(
     "mongodb+srv://yogeshshamlin:Nickyzander%40%32%30%30%34@cluster0.4a6cuu0.mongodb.net/RecYipeee?retryWrites=true&w=majority"
 )
 
-app.listen(3000, ()=>{
-    console.log("server started at PORT 3000");
+app.listen(3001, ()=>{
+    console.log("server started at PORT 3001");
 });
