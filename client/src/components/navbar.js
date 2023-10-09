5283d81adac44d6d6fb9ae1a720d9d2543ad38f6
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-09 14:01:15 +0530

    fixed major issues!

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,5 +1,6 @@
 import {Link} from "react-router-dom";
 import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 import 'font-awesome/css/font-awesome.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,12 +10,24 @@ import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 
 export const Navbar = ()=>{
+    const [cookies,setCookies] = useCookies(["access_token"]);
+    const navigate = useNavigate();
+    const logout = ()=>{
+        setCookies("access_token","");
+        window.localStorage.removeItem("userId");
+        navigate("/auth");
+    }
     return (
         <div className="navbar">
             <Link to="/" ><i className="fa fa-home"></i>Home</Link> 
             <Link to="/create-recipe" ><FontAwesomeIcon icon={faUtensils} />Create a RecYippee!</Link> 
             <Link to="/saved-recipes" ><FontAwesomeIcon icon={faBookmark} />Saved RecYippees</Link> 
-            <Link to="/auth" ><FontAwesomeIcon icon={faSignInAlt} />Login/Register</Link> 
+            {!cookies.access_token ? (
+                <Link to="/auth" ><FontAwesomeIcon icon={faSignInAlt} />Login/Register</Link> 
+                ): (
+                <button onClick ={logout}><FontAwesomeIcon icon={faSignInAlt} />Logout</button>
+                )}
+            
         </div>
     )
 }
