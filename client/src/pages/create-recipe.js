4f4490cd62cdfc0aa546e771c69e2ab4beef17cd
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-12 19:23:12 +0530

    created form!

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -14,9 +14,18 @@ export const CreateRecipe = ()=>{
         const {name,value}=event.target;
         setRecipe({...recipe, [name] : value})
     }
+    const handleIngredientChage = (event,idx)=>{
+        const {value}=event.target;
+        const ingredients = recipe.ingredients;
+        ingredients[idx]=value;
+        setRecipe({...recipe, ingredients})
+    }
+    const addIngredient = ()=>{
+        setRecipe({...recipe,ingredients: [...recipe.ingredients, ""]});
+    }
 
     return (
-        <div className="create-recipte">
+        <div className="create-recipe">
             <h2>Create a recipe!</h2>
             <form>
                 <label htmlFor="name" >Name</label>
@@ -27,7 +36,12 @@ export const CreateRecipe = ()=>{
                     onChange={handleChage} 
                 />
                 <label htmlFor="ingredients" >Ingredients</label>
-
+                {recipe.ingredients.map( (ingredient,idx)=>(
+                    <input key={idx} type="text" name="ingredients" value={ingredient}
+                    onChange={ (event)=>handleIngredientChage(event,idx) }
+                    />
+                ) )}
+                <button onClick={addIngredient} type="button">+</button>
                 <label htmlFor="instructions" >Instructions</label>
                 <textarea 
                 type="text" 
@@ -49,6 +63,7 @@ export const CreateRecipe = ()=>{
                     name="cookingTime" 
                     onChange={handleChage}
                 />
+                <button type="submit">Create RecYippee!</button>
             </form>
         </div>
     )
