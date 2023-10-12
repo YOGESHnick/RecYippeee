e6c4c6e4e8677f3a55272cdcc00d3333b4499830
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-10-12 18:06:46 +0530

    startig frontend for recipes

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -1,5 +1,17 @@
 export const CreateRecipe = ()=>{
     return (
-        <div>CreateRecipe</div>
+        <div className="create-recipte">
+            <h2>Create a recipe!</h2>
+            <form>
+                <label htmlFor="name" >Name</label>
+                <input type="text" id="name"  />
+                <label htmlFor="ingredients" >Ingredients</label>
+
+                <label htmlFor="instructions" >Instructions</label>
+                <textarea type="text" id="instructions" name="istructions" > </textarea>
+                <label htmlFor="imageUrl" >Image URL</label>
+                <input type="text" id="imageUrl"  />
+            </form>
+        </div>
     )
 }
